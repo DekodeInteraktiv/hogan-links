30c456fdd2c68809c39dea72c4fd7daabc096b43
pederan <peder1976@gmail.com>
2018-02-16 09:44:35 +0100

    v1.1.0

--- hogan-links.php
+++ hogan-links.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-links
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-links
  * Description: Links Module for Hogan
- * Version: 1.0.2
+ * Version: 1.1.0
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0-or-later
