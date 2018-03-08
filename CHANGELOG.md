c6e76df1c092dba6ffaf16da034b667f381e3027
Petter Walbø Johnsgård <petter@dekode.no>
2018-03-08 11:00:25 +0100

    v1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.1.1]
+- Added default classname to the `<ul>` and `<li>` elements
+- Added `hogan_links_after_text` after text action.
+
 ## [1.1.0]
 ### Breaking Changes
 - Remove heading field, provided from Core in [#53](https://github.com/DekodeInteraktiv/hogan-core/pull/53)
--- hogan-links.php
+++ hogan-links.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-links
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-links
  * Description: Links Module for Hogan
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0-or-later
